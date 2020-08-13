e525f1c6d1a1d44d7525641cf43ae72d16285854
mikefarias <mikesantosfarias@gmail.com>
2020-08-12 21:25:36 -0300

    registro de novo usuario do tipo company

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Company } from './models/company';
+import { FormGroup } from '@angular/forms';
+
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'register.component.html'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
-  constructor() { }
+  company = {} as Company;
+  companies: Company[];
 
+  constructor(private service: RegisterService) {}
+
+  ngOnInit() {
+  }
+
+  registerUserCompany(frm: FormGroup) {
+    this.service.postUserCompany(this.company).subscribe(response => {
+      this.companies.push(response);
+
+      frm.reset();
+    });
+  }
 }
---
+++ src/app/views/register/register.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Company } from './models/company';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  url = 'http://192.168.0.102:8080/api/v1/register/user/company';
+
+  // injetando o HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // Headers
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  // salva um user
+  postUserCompany(company: Company): Observable<Company> {
+      return this.httpClient.post<Company>(this.url, JSON.stringify(company), this.httpOptions)
+        .pipe(
+          retry(2),
+          catchError(this.handleError)
+        )
+    }
+  // Manipulação de erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
+
+}
